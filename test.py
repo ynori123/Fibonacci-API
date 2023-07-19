58411d36b40c2371a51ebcf6f9766a199bf613fe
ynori123 <norisama1159@gmail.com>
2023-07-19 15:09:58 +0900

    UnitTest

---
+++ test.py
@@ -0,0 +1,24 @@
+from app import fibonacci
+import unittest
+
+class Test(unittest.TestCase):
+    def test_first_fib(self):
+        result = fibonacci(1)
+        self.assertEqual(result, 1)
+    def test_second_fib(self):
+        result = fibonacci(2)
+        self.assertEqual(result, 1)
+    def test_third_fib(self):
+        result = fibonacci(3)
+        self.assertEqual(result, 2)
+    def test_fourth_fib(self):
+        result = fibonacci(4)
+        self.assertEqual(result, 3)
+    def test_fifth_fib(self):
+        result = fibonacci(5)
+        self.assertEqual(result, 5)
+    def test_large_fib(self):
+        result = fibonacci(100)
+        self.assertEqual(result, 354224848179261915075)
+if __name__=='__main__':
+    unittest.main()
